fb9bc4f361ad458f216e4930d18b3f023e61dc84
Tenzin Tenkyong <ttenkyong@gmail.com>
2021-10-01 16:24:40 -0400

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,41 @@
+function add (x,y) {
+    return x+y;
+}
+
+function subtract(x,y) {
+    return x-y;
+}
+
+function multiply(x,y) {
+    return x*y;
+}
+
+function divide(x,y) {
+    return x/y;
+}
+
+let number = 5;
+
+function increment(n) {
+    return n+1;
+}
+
+function decrement(n) {
+    return n-1;
+}
+
+function makeInt(a) {
+    return parseInt(a, 2);
+}
+
+function makeInt(a) {
+    return parseInt(a, 10);
+}
+function preserveDecimal(a) {
+   return parseFloat("2.222");
+}
+
+function preserveDecimal(a) {
+   return parseFloat(a);
+}
+
